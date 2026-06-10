9eb23bec7dc10b75dbe9d69b2081909e9fd7da05
Jonathan2055 <j.mugisha3@alustudent.com>
2026-06-10 19:03:28 +0200

    Created the search and header and greeting to the home page

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 12, 20, 100),
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
